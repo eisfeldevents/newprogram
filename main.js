3ecf200a36ddbbf72f32c781edf9efc018f0c394
heydominik <dercoolejunge333@gmail.com>
2022-05-18 14:59:59 +0200

    commi2

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 let einsatzSpieler1
 let einsatzSpieler2
 
-
+einsatz()
 function einsatz(){
     gesamtEinsatz = einsatzSpieler1 + einsatzSpieler2
 }
 let würfel1
 let würfel2
 
-function würfeln(){
+function würfelnsp1(){
 
     let min = 1;
     let max = 6;
     würfel1 = Math.round(Math.random() * (max - min)) + min;
     würfel2 = Math.round(Math.random() * (max - min)) + min;
-    
+    document.getElementById("aktWuerfel1").innerHTML = würfel1;
+    document.getElementById("aktWuerfel2").innerHTML = würfel2;
+    rechnen()
 
 }
 
@@ -27,12 +29,12 @@ let Rundenanzahl = 1
 function rechnen(){
     augenzahlsp1= augenzahlsp1 + würfel1 + würfel2
 
-    if (augenzahl == 7) {
+    if (augenzahlsp1 == 7) {
         console.log("Runde vorbei")
 
     }
     else if(nochEineRunde == 1){
-        würfeln()
+        würfelnn()
         Rundenanzahl = Rundenanzahl + 1
     }
 
@@ -43,16 +45,17 @@ function rechnen(){
     }
 }
 i = 0;
-function rechnensp2(){
-    if (i <= Rundenanzahl)
-    würfeln()
-    augenzahlsp2 = augenzahlsp2 + würfel1 + würfel2
-else if(nochEineRunde == 1){
-        würfeln()
-        Rundenanzahl = Rundenanzahl + 1
+function rechnensp2() {
+    if (i <= Rundenanzahl) {
+        würfelnn()
+        augenzahlsp2 = augenzahlsp2 + würfel1 + würfel2
     }
-else{
-    auswertung()
+else
+    if (nochEineRunde == 1) {
+        würfelnn()
+        Rundenanzahl = Rundenanzahl + 1
+    } else {
+        auswertung()
     }
 
 }
